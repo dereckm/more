5dbb9b38af3e935b0d95f99455fc39c108a112cc
Dereck <ak90730@ens.etsmtl.ca>
2023-05-19 14:07:47 -0400

    Damerau-Levenshtein distance

---
+++ More.Strings.Tests/Calculators/DamerauLevenshteinDistanceCalculatorTests.cs
@@ -0,0 +1,96 @@
+﻿using FluentAssertions;
+using More.Strings.Calculators;
+using More.Strings.Models;
+using NUnit.Framework;
+
+namespace More.Strings.Tests.Calculators;
+
+[TestFixture]
+public class DamerauLevenshteinDistanceCalculatorTests
+{
+    private DamerauLevenshteinDistanceCalculator _calculator;
+
+    [SetUp]
+    public void Setup()
+    {
+        _calculator = new DamerauLevenshteinDistanceCalculator();
+    }
+
+    [Test]
+    public void Calculate_WhenSourceAndTargetAreEqual_ShouldReturnZero()
+    {
+        // Arrange
+        var input = new StringDistanceInput("hello", "hello");
+
+        // Act
+        var distance = _calculator.Calculate(input);
+
+        // Assert
+        distance.Should().Be(0);
+    }
+
+    [Test]
+    public void Calculate_WhenSourceIsEmpty_ShouldReturnTargetLength()
+    {
+        // Arrange
+        var input = new StringDistanceInput("", "world");
+
+        // Act
+        var distance = _calculator.Calculate(input);
+
+        // Assert
+        distance.Should().Be(5);
+    }
+
+    [Test]
+    public void Calculate_WhenTargetIsEmpty_ShouldReturnSourceLength()
+    {
+        // Arrange
+        var input = new StringDistanceInput("hello", "");
+
+        // Act
+        var distance = _calculator.Calculate(input);
+
+        // Assert
+        distance.Should().Be(5);
+    }
+
+    [Test]
+    public void Calculate_WhenSourceAndTargetHaveDifferentCharacters_ShouldReturnCorrectDistance()
+    {
+        // Arrange
+        var input = new StringDistanceInput("kitten", "sitting");
+
+        // Act
+        var distance = _calculator.Calculate(input);
+
+        // Assert
+        distance.Should().Be(3);
+    }
+
+    [Test]
+    public void Calculate_WhenSourceAndTargetAreCompletelyDifferent_ShouldReturnMaxLength()
+    {
+        // Arrange
+        var input = new StringDistanceInput("abc", "def");
+
+        // Act
+        var distance = _calculator.Calculate(input);
+
+        // Assert
+        distance.Should().Be(3);
+    }
+
+    [Test]
+    public void Calculate_WhenSourceAndTargetContainTranspositions_ShouldReturnCorrectDistance()
+    {
+        // Arrange
+        var input = new StringDistanceInput("abcdef", "bacdfe");
+
+        // Act
+        var distance = _calculator.Calculate(input);
+
+        // Assert
+        distance.Should().Be(2);
+    }
+}
---
+++ More.Strings/Calculators/DamerauLevenshteinDistanceCalculator.cs
@@ -0,0 +1,46 @@
+﻿using More.Core;
+using More.Strings.Models;
+
+namespace More.Strings.Calculators;
+
+public class DamerauLevenshteinDistanceCalculator : ICalculator<StringDistanceInput, int>
+{
+    public int Calculate(StringDistanceInput input)
+    {
+        var source = input.Source;
+        var target = input.Target;
+        var sourceLength = source.Length;
+        var targetLength = target.Length;
+
+        if (sourceLength == 0)
+            return targetLength;
+        if (targetLength == 0)
+            return sourceLength;
+
+        var distanceMatrix = new int[sourceLength + 1, targetLength + 1];
+        for (var i = 0; i <= sourceLength; i++)
+            distanceMatrix[i, 0] = i;
+        for (var j = 0; j <= targetLength; j++)
+            distanceMatrix[0, j] = j;
+
+        for (var i = 1; i <= sourceLength; i++)
+        {
+            for (var j = 1; j <= targetLength; j++)
+            {
+                var cost = source[i - 1] == target[j - 1] ? 0 : 1;
+                var deletion = distanceMatrix[i - 1, j] + 1;
+                var insertion = distanceMatrix[i, j - 1] + 1;
+                var substitution = distanceMatrix[i - 1, j - 1] + cost;
+
+                distanceMatrix[i, j] = Math.Min(Math.Min(deletion, insertion), substitution);
+
+                if (i > 1 && j > 1 && source[i - 1] == target[j - 2] && source[i - 2] == target[j - 1])
+                {
+                    distanceMatrix[i, j] = Math.Min(distanceMatrix[i, j], distanceMatrix[i - 2, j - 2] + cost);
+                }
+            }
+        }
+
+        return distanceMatrix[sourceLength, targetLength];
+    }
+}
